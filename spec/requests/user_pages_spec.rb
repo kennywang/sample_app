370202dfe0ad15db0cdef9d7c00ff9651eb9a0aa
Kenny Wang <kwang@constantcontact.com>
2012-11-14 10:35:49 -0500

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,70 @@ describe "User pages" do
 
   subject { page }
 
+	before do
+	    @user = User.new(name: "Example User", email: "user@example.com", 
+	                     password: "foobar", password_confirmation: "foobar")
+	end
+	
+	describe "profile page" do
+	    let(:user) { FactoryGirl.create(:user) }
+	    before { visit user_path(user) }
+
+	    it { should have_selector('h1',    text: user.name) }
+	    it { should have_selector('title', text: user.name) }
+	end
+	
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_selector('title', text: 'Sign up') }
   end
+
+	describe "when email format is invalid" do
+	  it "should be invalid" do
+	    addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+	                     foo@bar_baz.com foo@bar+baz.com]
+	    addresses.each do |invalid_address|
+	        @user.email = invalid_address
+	        @user.should_not be_valid
+	    end      
+	  end
+	end
+
+	describe "when email format is valid" do
+	  it "should be valid" do
+	    addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+	    addresses.each do |valid_address|
+	        @user.email = valid_address
+	        @user.should be_valid
+	    end      
+	  end
+	end
+	
+	describe "signup" do
+
+	    before { visit signup_path }
+
+	    let(:submit) { "Create my account" }
+
+	    describe "with invalid information" do
+	      it "should not create a user" do
+	        expect { click_button submit }.not_to change(User, :count)
+	      end
+	    end
+
+	    describe "with valid information" do
+	      before do
+	        fill_in "Name",         with: "Example User"
+	        fill_in "Email",        with: "user@example.com"
+	        fill_in "Password",     with: "foobar"
+	        fill_in "Confirmation", with: "foobar"
+	      end
+
+	      it "should create a user" do
+	        expect { click_button submit }.to change(User, :count).by(1)
+	      end
+	    end
+	end
 end
